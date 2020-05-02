543113c96671ebe37fa94e37bfd05661ce8e7824
Sonali Hirave <hiravesonali@gmail.com>
2020-05-02 18:09:21 +0200

    add fibonacci file

---
+++ Basic/fibonacci-sequence.php
@@ -0,0 +1,21 @@
+<?php
+function fibonacci($maxNumber, $first = 0,$second = 1) {
+
+    $fib = [$first, $second];
+
+    for($i=1; $second < $maxNumber; $i++) {
+        $second = $fib[$i] + $fib[$i-1];
+        
+        if ($second > $maxNumber) {
+            continue;
+        }
+
+        $fib[] = $second;
+    }
+
+    return $fib;
+}
+
+echo "<pre>";
+
+print_r(fibonacci(2000));
