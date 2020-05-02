bd3c1438630235d82a03cd9db695e136b8ccf464
Sonali Hirave <hiravesonali@gmail.com>
2020-05-02 22:44:20 +0200

    ascending character print

--- Basic/print.php
+++ Basic/print.php
@@ -1,8 +1,26 @@
 <?php
 
+function descending($max) {
 
-function ascending($max) {
+    for ($x = $max; $x > 0; $x--) {
+    
+        for ($y = $x; $y > 0; $y--) {
+            echo "* ";
+        }
 
+        echo  "<br>";
+    }
 }
 
+function ascending($max, $char = 'S') {
+
+    for($x = 1; $x <= $max; $x++) {
+        for ($y = 1; $y <= $x; $y++){        
+            echo "$char ";
+        }
+        echo "<br>";
+    }
+}
+
+ascending(5, 'Sonali');
 descending(5);
