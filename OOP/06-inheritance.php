f860238f2fb3b5e1215beaf1b0a3df188eda7f04
Sonali Hirave <hiravesonali@gmail.com>
2020-05-03 23:17:26 +0200

    inheritance tested

---
+++ OOP/06-inheritance.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * public - the property or method can be accessed from everywhere. This is default
+ * protected - the property or method can be accessed within the class and by classes derived from that class
+ * private - the property or method can ONLY be accessed within the class
+ */
+
+class Animal {
+
+    public $name;
+
+    private $type;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setType($type)
+    {
+        if ($type == 'Cat') {
+            $this->type = $type;
+        } else {
+            echo 'Hey, I told you to have cats only';
+        }
+    }
+
+    protected function getType()
+    {
+        return $this->type;
+    }
+}
+
+class Pet extends Animal
+{
+    public $cloths;
+
+    public function printDetails()
+    {
+        echo $this->name . ' is a ' . $this->getType().' who likes '. $this->cloths.' cloths <br>';
+    }
+}
+
+$pet = new Pet('Sweetie');
+
+$pet->setType('Cat');
+$pet->cloths = ' Red';
+
+echo $pet->printDetails();
