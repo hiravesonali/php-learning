86b40546c5c8f19ca4051824350b527882989073
Sonali Hirave <hiravesonali@gmail.com>
2020-05-08 14:29:59 +0200

    Basic form

--- test.php
+++ test.php
@@ -1,56 +1,16 @@
 <?php
 
-trait AnimalInfo
-{
-    public function info()
-    {
-        return "I am $this->type, and my name is $this->name!";
+function ascending($max){
+     
+    for ($x = 1; $x <= $max; $x++){
+
+        for ($y = 1; $y <= $x; $y++){
+
+            echo "S";
+        }
+
+        echo "<br>";
     }
 }
 
-abstract class Animal
-{
-    public $name;
-    protected $type;
-    
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-
-    // Subclasses must have to have abstract methods
-    abstract public function info();
-}
-
-// child classes
-class Cat extends Animal
-{
-    use AnimalInfo;
-
-    public function __construct($name)
-    {
-        parent::__construct($name);
-
-        $this->type = 'Cat';
-    }
-}
-
-class Dog extends Animal
-{
-    use AnimalInfo;
-
-    public function __construct($name)
-    {
-        parent::__construct($name);
-
-        $this->type = 'Dog';
-    }
-}
-
-
-$cat = new Cat('Sweetie');
-echo $cat->info();
-echo "<br>";
-
-$dog = new Dog('Tomi');
-echo $dog->info();
+ascending(5);
