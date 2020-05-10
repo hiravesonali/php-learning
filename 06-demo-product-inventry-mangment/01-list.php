88a3a4bf0254f923aa8811436963441d3856f3bb
Sonali Hirave <hiravesonali@gmail.com>
2020-05-10 15:12:01 +0200

    PIM select data form database

--- 06-demo-product-inventry-mangment/01-list.php
+++ 06-demo-product-inventry-mangment/01-list.php
@@ -18,24 +18,7 @@ $dbname = "PIM";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO Products (EAN, Name, Category, Description)
-VALUES ('345758','John', 'Gents Sport', 'Mens fashion')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
-
-
 
 <nav class="flex items-center justify-between flex-wrap bg-indigo-500 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -72,7 +55,6 @@ $conn->close();
         Add Product
         </button>
     </div>
-
     <table class="border-collapse w-full">
         <thead>
             <tr>
@@ -85,21 +67,26 @@ $conn->close();
             </tr>
         </thead>
         <tbody>
+        <?php $sql = "SELECT * FROM Products";
+            $result = $conn->query($sql); 
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+            ?>
             <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
                 <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                    01
+                    <?=$row["id"]?>
                 </td>
                 <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    12585
+                    <?=$row["EAN"]?>
                 </td>
                 <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Ramesh
+                    <?=$row["Name"]?>
                 </td>
                 <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    <span class="rounded bg-red-400 py-1 px-3 text-xs font-bold">Home Decore</span>
+                    <span class="rounded bg-red-400 py-1 px-3 text-xs font-bold"><?=$row["Category"]?></span>
                 </td>
                 <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    All Home decore.
+                    <?=$row["Description"]?>
                 </td>
                 <td class="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static items-center">
                     <div class="flex justify-center items-center">
@@ -113,93 +100,8 @@ $conn->close();
                     </div>
                 </td>
             </tr>
-
-            <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                    02
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    12345
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Vikas
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    <span class="rounded bg-blue-400 py-1 px-3 text-xs font-bold">Gent's Sport</span>
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Men's fashion, Shoes, T-shirt, Jeans.
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static items-center">
-                    <div class="flex justify-center items-center">
-                        <a href="#" class="text-blue-400 hover:text-blue-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-
-                        <a href="#" class="text-red-400 hover:text-red-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-                    </div>
-                </td>
-            </tr>
-
-            <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                    03
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    15486
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Sonali
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    <span class="rounded bg-yellow-400 py-1 px-3 text-xs font-bold">Ladies Fashion</span>
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Ladies fashion, Shoes, T-shirt, Jeans.
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static items-center">
-                    <div class="flex justify-center items-center">
-                        <a href="#" class="text-blue-400 hover:text-blue-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-
-                        <a href="#" class="text-red-400 hover:text-red-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-                    </div>
-                </td>
-            </tr>
-           
-            <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                    04
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    15386
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Shlok
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    <span class="rounded bg--400 py-1 px-3 text-xs font-bold">Kids Wear</span>
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-                    Kids fashion, Shoes, T-shirt, Jeans, Toys.
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 border border-b text-center block lg:table-cell relative lg:static items-center">
-                    <div class="flex justify-center items-center">
-                        <a href="#" class="text-blue-400 hover:text-blue-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-
-                        <a href="#" class="text-red-400 hover:text-red-600">
-                            <svg class= "h-8 w-8" fill="currentColor" viewBox="0 0 20 20"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                        <a>
-                    </div>
-                </td>
-            </tr>
+                <?php }
+            } ?>
         </tbody>
     </table>
 </div>
