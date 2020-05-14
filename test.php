8b0d02f7067ea6238af9345ac19351ff2f960b7c
Sonali Hirave <hiravesonali@gmail.com>
2020-05-14 09:49:30 +0200

    twitter UI demo design

--- test.php
+++ test.php
@@ -1,16 +1,2 @@
 <?php
 
-function ascending($max){
-     
-    for ($x = 1; $x <= $max; $x++){
-
-        for ($y = 1; $y <= $x; $y++){
-
-            echo "S";
-        }
-
-        echo "<br>";
-    }
-}
-
-ascending(5);
