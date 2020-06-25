7b23f6af1fa7cc6397bc357047c285fe56d7644c
Sonali Hirave <hiravesonali@gmail.com>
2020-06-25 22:08:04 +0200

    Overloading & override functions

--- 02-OOP/12-overloading.php
+++ 02-OOP/12-overloading.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  Function from parent class is re-written in child class.
+ *  Function from parent class is re-written(extended) in the child class.
  */
 
  abstract class Resident
@@ -13,32 +13,39 @@
  {
      public function bloodType()
      {
+         // This function is only defined in the base class & will be inherited to child class
          return 'depends upon person';
      }
 
      public function food()
      {
+         // will be override in the child classes
          return 'Different everywhere';
-     }
-
-     public function culture()
-     {
-         return 'different in continents';
      }
 
      public function taxes($income)
      {
+         // will be overloaded in the child classes
          $rate = 10;
          return ($rate/100) * $income;
      }
  }
 
+ 
  class GermanResident extends WorldResident 
  {
-     //Overloading
+     // Inherited other functions
+
+     // Overriding - same number of params/arguments passed but definition changed
+    public function food()
+    {
+        return 'curry wurst';
+    }
+
+     //Overloading - different number of params/arguments passed
     public function taxes($income, $churchTaxes)
     {
-        $rate = 40+$churchTaxes;
+        $rate = 40 + $churchTaxes;
         return ($rate/100) * $income;
     }
  }
